9f1be57b901ef988bdc78496ea558f41774305e0
Eric Rose <me@ericrose.me>
2016-04-17 17:45:42 -0400

    increased message count to fit websurface

--- logger.js
+++ logger.js
@@ -15,7 +15,7 @@ function addMessage(msg){
     li.innerHTML = msg;
     messageList.prepend(li);
     
-    if($("#messages li").length > 45){
+    if($("#messages li").length > 55){
         $('#messages li:last').remove();
     }
 }
